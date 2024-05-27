9b44ea75f3c96ae08a0ab4d679d4f48f9f1fc7f7
cmpunk-best <vaibhavraj.v11@gmail.com>
2024-05-27 21:52:19 +0530

    Algo dir added

---
+++ calc/algo/newton_zero.py
@@ -0,0 +1,47 @@
+
+
+"""
+Newton method for finding zeros
+1. Recursive method.
+2. Finding initial guess (c1)
+3. Finding c2 from eq
+    y - f(c1) = f'(c1) * (x - c1)
+    plug y = 0
+    -f(c1) = f'(c1) * (c2-c1)
+    c1 - f(c1)/f'(c1) = c2 
+    Basically,
+    c_n = c_n-1 - f(c_n-1)/f'(c_n-1)
+    c_n+1 = c_n - f(c_n)/f'(c_n)
+
+4. Guessing c_n-1 is the toughest part
+"""
+import sympy as sym
+
+def d_f(fun, symbol = 'x', times = 1):
+    return sym.diff(fun, symbol, times)
+
+def newton_zero(fun,c_n,symbol='x'):
+    c_n_1 = c_n
+    if(sym.lambdify(symbol,fun)(c_n) <= 0.002):
+        print("DONE",c_n)
+        return c_n
+    else:
+        c_n_1 = c_n - (sym.lambdify(symbol,fun)(c_n)/sym.lambdify(symbol,d_f(fun))(c_n))
+        print(c_n_1)
+        newton_zero(fun,c_n_1)
+
+# Functions
+x = sym.symbols('x')
+fun = x**3 + x**2 - x - 2 
+k = newton_zero(fun, c_n=1.5)
+print(k)
+
+
+
+
+
+
+
+
+
+
